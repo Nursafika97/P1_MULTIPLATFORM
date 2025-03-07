f6b7f090a81fcff99e62ba56160c9d0cef7dfaec
Nursafika97 <nursafikaefendi@gmail.com>
2025-03-07 11:38:44 +0700

    ketiga

--- ex3_ketiga.dart
+++ ex3_ketiga.dart
@@ -1,6 +1,9 @@
 void main() {
+  // Deklarasi variabel bertipe String
   String polbeng = 'Politeknik Negeri Bengkalis';
-  String ti = 'Tekink Informatika';
+  String ti = 'Teknik Informatika';
   String rpl = 'Rekayasa Perangkat Lunak';
+
+  // Mencetak output dengan string interpolation
   print('Saya kuliah di $polbeng jurusan $ti program studi $rpl');
 }
